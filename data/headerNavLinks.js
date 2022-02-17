80c338d56e5de6728e89fa3562bca7175e6d1060
indicozy <indicozy@protonmail.com>
2022-02-17 17:14:09 +0600

    kkk

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -5,7 +5,8 @@ const headerNavLinks = [
   { href: '/about', title: 'О Проекте' },
   { href: 'https://edu.op-onai.kz', title: 'Курсы', highlight: true },
   { href: 'https://forum.op-onai.kz', title: 'Форум', highlight: true },
-  { href: 'https://taiga.op-onai.kz', title: 'Agile', highlight: true },
+  { href: 'https://agile.op-onai.kz', title: 'Agile', highlight: true },
+  { href: 'https://design.op-onai.kz', title: 'Дизайн', highlight: true },
   { href: 'https://discord.gg/kag4mXFM7y', title: 'Discord', highlight: true },
 ]
 

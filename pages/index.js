36a552f7e81339cba8c3d78b0e69299630c67efa
indicozy <indicozy@protonmail.com>
2022-02-21 13:02:21 +0600

    improved front

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
+// import Image from 'next/image'
 
 import NewsletterForm from '@/components/NewsletterForm'
 
@@ -20,30 +21,93 @@ export default function Home({ posts }) {
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-          Научись программировать. Бесплатно.
+        <h1 className="text-3xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Научись программировать.{' '}
+          <span className="underline decoration-primary-500">Бесплатно</span>.
         </h1>
-        <h2 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <h2 className="text-3xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           Создавай Проекты.
         </h2>
-        <h2 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <h2 className="text-3xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           Получай Сертификаты.
         </h2>
         <p className="text-xl leading-7 text-gray-500 dark:text-gray-300">
-          {siteMetadata.description}
+          Онлайн открытые курсы и сообщество о навыках в индустрии IT. Начни свое путешествие в
+          разработке с Op-Onai.
         </p>
-        <div className="mb-10 sm:mb-20">
+        <div className="mb-20 pt-10 sm:mb-20">
           <a
             href="https://edu.op-onai.kz"
-            className="rounded bg-black px-4 py-1 text-sm text-gray-100 ring-offset-indigo-700 transition duration-150 ease-in-out hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 dark:bg-white dark:text-gray-900 sm:px-8 sm:py-4 lg:text-xl"
+            className="rounded bg-black px-4 py-1 text-sm font-bold text-gray-100 ring-offset-indigo-700 transition duration-150 ease-in-out hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 dark:bg-white dark:text-gray-900 sm:px-8 sm:py-4 lg:text-xl"
           >
             Начать
           </a>
         </div>
       </div>
+      <div className="my-10 my-20 grid place-items-center">
+        <iframe
+          width="90%"
+          height="500px"
+          src="https://www.youtube.com/embed/8jLOx1hD3_o"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="my-20">
+        <h2 className="text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Курсы: Op-Onai camp
+        </h2>
+        <div className="my-10 grid place-items-center">
+          <img alt="" className="px-10 pt-5" src="/static/images/edu-op-onai.png" />
+        </div>
+        <p className="pb-5 text-xl leading-7 text-gray-500 dark:text-gray-300">
+          100% Беслатные курсы с возможностью получения сертификатов. Смотри, изучай, проходи тесты
+          и создавай проекты.
+        </p>
+      </div>
+      <div className="my-20">
+        <h2 className="text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Сообщество: Discord, Форум
+        </h2>
+        <div className="my-10 grid place-items-center">
+          <img alt="" className="px-10 pt-5" src="/static/images/forum-op-onai.png" />
+        </div>
+        <p className="pb-5 text-xl leading-7 text-gray-500 dark:text-gray-300">
+          Задавай вопросы, помогай другим. Находи единомышленников и начинайте проекты вместе.
+        </p>
+      </div>
+      <div className="my-20">
+        <h2 className="text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Коллаборации: Agile + Design
+        </h2>
+        <div className="my-10 grid place-items-center">
+          <img alt="" className="px-10 pt-5" src="/static/images/agile-op-onai.png" />
+          <img alt="" className="px-10 pt-5" src="/static/images/design-op-onai.png" />
+        </div>
+        <p className="pb-5 text-xl leading-7 text-gray-500 dark:text-gray-300">
+          Запускай проекты в профессинальной платформе. Создавай цели, составляйте планы и стройте
+          приложения будущего.
+        </p>
+      </div>
+      <div className="my-20">
+        <h2 className="my-10 text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Чего ты ждешь? Начни <span className="underline decoration-primary-500">сейчас.</span>
+        </h2>
+        <p className="my-10 pb-5 text-xl leading-7 text-gray-500 dark:text-gray-300">
+          Начать можешь с бесплатных курсов. Заходи в edu.op-onai.kz и открой для себя мир IT.
+        </p>
+        <a
+          href="https://edu.op-onai.kz"
+          className="my-10 rounded bg-black px-4 py-5 text-sm font-bold text-gray-100 ring-offset-indigo-700 transition duration-150 ease-in-out hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 dark:bg-white dark:text-gray-900 sm:px-8 sm:py-4 lg:text-xl"
+        >
+          Начать
+        </a>
+      </div>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h2 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h2 className="text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Последние Новости
           </h2>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
